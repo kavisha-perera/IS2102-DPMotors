1f59713c62ad088103671a771be1b0dd09e25ad7
Janani2102 <2019is021@stu.ucsc.cmb.ac.lk>
2021-10-16 03:58:38 +0530

    UIs for Promotions and appointments
    uploaded the create, read, update, delete UIs for Promotions and appointments.

---
+++ javascript/promotionPopup.js
@@ -0,0 +1,11 @@
+/*Open and Close of deleted promotion message*/
+function OnClickOpenDeletePromotion()
+{
+    document.getElementById('th-delete-promotion').style.display = "block";
+}
+
+function OnClickCloseDeletePromotion()
+{
+    document.getElementById('th-delete-promotion').style.display = "none";
+}
+
