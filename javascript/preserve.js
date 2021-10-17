642ed435676a37aea7ccb50b1c72c22a1e5f5a91
TharushiAlgama <2019is005@stu.ucsc.cmb.ac.lk>
2021-10-16 17:39:27 -0700

    Update preserve.js

--- javascript/preserve.js
+++ javascript/preserve.js
@@ -7,3 +7,33 @@ function OnClickCloseUpdateService()
     {
         document.getElementById('th-update-service').style.display = "none";  
     }
+
+    function OnclickOpenDeleteService()
+    {
+        document.getElementById('th-pr-delete').style.display = "block";  
+    }
+
+    function OnclickCloseDeleteService()
+    {
+        document.getElementById('th-pr-delete').style.display = "none";  
+    }
+
+    function OnClickOpenUpdatePR()
+    {
+        document.getElementById('th-update-pr').style.display = "block";  
+    }
+
+    function OnclickCloseDeleteUpdatePR()
+    {
+        document.getElementById('th-update-pr').style.display = "none";  
+    }
+
+    function OnClickOpenDeleteReserve()
+    {
+        document.getElementById('th-delete-preser').style.display = "block";  
+    }
+
+    function onClickCloseDeleteReserve()
+    {
+        document.getElementById('th-delete-preser').style.display = "none";  
+    }
