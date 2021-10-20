f37bde127e7a4b62986bcab692e97c8a56fbeac8
TharushiAlgama <92346310+TharushiAlgama@users.noreply.github.com>
2021-10-21 02:15:50 +0530

    Add files via upload

--- javascript/empsup_pop-up.js
+++ javascript/empsup_pop-up.js
@@ -1,67 +1,53 @@
 /*Open and Close of New Supplier form*/
-function onClickOpenAddSupplier()
-{
-    document.getElementById('th-add-new-supplier').style.display = "block";  
+function onClickOpenAddSupplier() {
+  document.getElementById("th-add-new-supplier").style.display = "block";
 }
 
-function OnClickCloseAddSupplier()
-{
-    document.getElementById('th-add-new-supplier').style.display = "none";
+function OnClickCloseAddSupplier() {
+  document.getElementById("th-add-new-supplier").style.display = "none";
 }
 
 /*Open and Close of Update Supplier form*/
-function OnClickOpenUpdateSupplier()
-{
-    document.getElementById('th-update-supplier').style.display = "block";
+function OnClickOpenUpdateSupplier() {
+  document.getElementById("th-update-supplier").style.display = "block";
 }
 
-function OnClickCloseUpdateSupplier()
-{
-    document.getElementById('th-update-supplier').style.display = "none";
+function OnClickCloseUpdateSupplier() {
+  document.getElementById("th-update-supplier").style.display = "none";
 }
 
 /*Open and Close of Delete Supplier message*/
-function OnClickOpenDeleteSupplier()
-{
-    document.getElementById('th-delete-supplier').style.display = "block";
+function OnClickOpenDeleteSupplier() {
+  document.getElementById("th-delete-supplier").style.display = "block";
 }
 
-function OnClickCloseDeleteSupplier()
-{
-    document.getElementById('th-delete-supplier').style.display = "none";
+function OnClickCloseDeleteSupplier() {
+  document.getElementById("th-delete-supplier").style.display = "none";
 }
 
 /*Open and Close of New Employee form*/
-function OnClickOpenAddEmloyee()
-{
-    document.getElementById('th-add-employee').style.display = "block";
+function OnClickOpenAddEmloyee() {
+  document.getElementById("th-add-employee").style.display = "block";
 }
 
-function OnClickCloseAddEmployee()
-{
-    document.getElementById('th-add-employee').style.display = "none";
+function OnClickCloseAddEmployee() {
+  document.getElementById("th-add-employee").style.display = "none";
 }
 
 /*Open and Close of employee update form*/
-function OnClickOpenUpdateEmployee()
-{
-    document.getElementById('th-update-employee').style.display = "block";
+function OnClickOpenUpdateEmployee(empno) {
+  //   document.getElementById("th-update-employee").style.display = "block";
 }
 
-function OnClickCloseUpdateEmployee()
-{
-    document.getElementById('th-update-employee').style.display = "none";
+function OnClickCloseUpdateEmployee() {
+  document.getElementById("th-update-employee").style.display = "none";
 }
 
 /*Open and Close of delete employee message*/
-function OnClickOpenDeleteEmployee()
-{
-    document.getElementById('th-delete-employee').style.display = "block";
+function OnClickOpenDeleteEmployee(empno) {
+  document.getElementById("th-delete-employee").style.display = "block";
 }
 
-function OnClickCloseDeleteEmployee()
-{
-    document.getElementById('th-delete-employee').style.display = "none";
+function OnClickCloseDeleteEmployee() {
+  document.getElementById("th-delete-employee").style.display = "none";
 }
-
-
