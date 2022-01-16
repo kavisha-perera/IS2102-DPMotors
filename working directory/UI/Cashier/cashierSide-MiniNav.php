9a1cd58362a14bdd84776a1aa4cc0b14819e1f54
TharushiAlgama <2019is005@stu.ucsc.cmb.ac.lk>
2022-01-16 13:08:33 +0700

    Changes in profile

---
+++ working directory/UI/Cashier/cashierSide-MiniNav.php
@@ -0,0 +1,42 @@
+<div class="col-2 sideNav-dropdown" >
+
+    <img src="../../../images/dropdown.svg" class="dropButton hide-in-others">
+    <img src="../../../images/dropdown.svg" class="dropButton hide-in-dashboard">
+
+    <div class="dropdown-content">
+    
+    <div class="hide-in-others">
+<a href="../dashboards/cashierDash.php" class="Nav-dashboard"> Dashboard </a> <hr>  
+</div>
+
+<div class="hide-in-dashboard">
+<a href="../../dashboards/cashierDash.php" class="Nav-dashboard"> Dashboard </a> <hr>  
+</div>
+
+<div class="hide-in-dashboard">
+    <a href="../cashierProfile/cashierViewProfile1.php" > Profile </a><hr> 
+    <a href="../cashierbills/createbill.php"> Create Bill </a>
+    <a href="../promotion/cashierReadPromotion.php"> Promotions </a>
+    <a href="../Cashier View Bill History/CashierViewAllBills.php"> Bill History </a> 
+    <a href="../Cashier service records/cashierViewService.php"> Vehicle Service Records </a> 
+    <a href="../Cashier product reservation/ViewProductResrvation.php"> Product Reservations </a>
+    <a href="../appointments/cashierReadsAppointments.php"> Appointments </a>
+    <a href="../Cashier Customer register/cashier register customer.php"> Customer </a>
+</div>
+
+<div class="hide-in-others">
+    <a href="../cashierProfile/cashierViewProfile1.php" > Profile </a><hr> 
+    <a href="../cashierbills/createbill.php"> Create Bill </a>
+    <a href="../promotion/cashierReadPromotion.php"> Promotions </a>
+    <a href="../Cashier View Bill History/CashierViewAllBills.php"> Bill History </a> 
+    <a href="../Cashier service records/cashierViewService.php"> Vehicle Service Records </a> 
+    <a href="../Cashier product reservation/ViewProductResrvation.php"> Product Reservations </a>
+    <a href="../appointments/cashierReadsAppointments.php"> Appointments </a>
+    <a href="../Cashier/Cashier Customer register/cashier register customer.php"> Customer </a>
+</div>
+
+    </div>
+</div>
+<div class="col-10 smallWel">
+    <p> Welcome <?php echo $email;?> </p>
+</div>
--- working directory/UI/dashboards/cashierDash.php
+++ working directory/UI/dashboards/cashierDash.php
@@ -20,58 +20,65 @@ if($_SESSION['type'] == "cashier")
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--https://www.w3schools.com/css/css_rwd_viewport.asp-->
     <link rel="stylesheet" href="../../css/main.css">
 	<title>cashier dashboard</title>
+    <style>
+        .Nav-dashboard{
+            /* to show the active link in navbar */
+            background-color:#344CB4; 
+        }
+        .hide-in-dashboard{
+            display:none;
+        }
+    </style>
 </head>
 <body>
 
-    <div class="row r1">
-        <div class="col-13">
-            <img src="../../images/logo.png" class="navLogo">
-        </div>
-        <div class="col-nav">
-            <h4 class="navSlogan">Dealers in all kinds of motor vehicle spare parts & accessories</h4>
-        </div>
-        <div class="col-14 navbar"> 
-        <form action="../../includes/logout-inc.php">
-                <button class="navButton"> Log Out </button>
-            </form>
-        </div>
+   
+<div class="row r1">
+    <div class="col-13">
+    <img src="../../images/logo.png" class="navLogo">
+</div>
+<div class="col-nav">
+    <h4 class="navSlogan">Dealers in all kinds of motor vehicle spare parts & accessories</h4>
+</div>
+<div class="col-14 navbar"> 
+        <?php 
+            if (isset($_SESSION['id'])){
+                echo "
+                <form action='../../includes/logout.inc.php'>
+                <button class='navButton'> Log Out </button>
+                </form> 
+                &nbsp;&nbsp; 
+                <form action='../dashboards/cashierDash.php'>
+                <button style='border:0px;cursor:pointer;'> <img src='../../images/profile-login.png' style='max-width:35px;'></button>
+                </form>
+                ";   
+             }
+
+             else {
+                echo "<form action='../Auth-UI/login.php'>
+                <button class='navButton'> Log In </button>
+                </form>
+                <form action='#contact'>
+                <button class='navButton contact'> Contact Us </button>
+                </form>";
+                 
+             }
+
+             ?>
+
+</div>
     </div>
+<!-- Start of Dropdown for screens with width less than 800px-->
+<div class="row r2">
+        <?php include_once("../Cashier/cashierSide-MiniNav.php") ?>
+    </div>
+<!--End of Dropdown for screens with width less than 800px-->
 
-    <!-- Start of Dropdown for screens with width less than 800px-->
-                    <div class="row r2">
-                        <div class="col-2 sideNav-dropdown" >
-                                <img src="../../images/dropdown.svg" class="dropButton">
-                                <div class="dropdown-content">
-                                    <a href="../dashboards/cashierDash.php"> Dashboard </a> 
-                                    <a href="../profiles/cashierViewProfile.php"> Profile </a>
-                                    <a href="../cashierbills/createbill.php"> Create Bill </a>
-                                    <a href="../promotion/cashierReadPromotion.php"> Promotions </a>
-                                    <a href="../Cashier/Cashier View Bill History/CashierViewAllBills.php"> Bill History </a> <!--*-->
-                                    <a href="../Cashier service records/cashierViewService.php"> Vehicle Service Records </a> 
-                                    <a href="../Cashier product reservation/ViewProductResrvation.php"> Product Reservations </a>
-                                    <a href="../appointments/cashierReadsAppointments.php"> Appointments </a>
-                                    <a href="../Cashier Customer register/cashier register customer.php"> Customer </a>
-                                </div>
-                        </div>
-                        <div class="col-10 smallWel">
-                            <p> Welcome @ <?php echo  $email ?></p>
-                        </div>
-                    </div>
-    <!--End of Dropdown for screens with width less than 800px-->
-
-    <div class="row r3">
+<div class="row r3">
         <div class="col-15 sideNav">
-            <p> Welcome @ <?php echo  $email ?></p> <hr>
-            <a href="../dashboards/cashierDash.php"> Dashboard </a><hr> 
-            <a href="../profiles/cashierViewProfile.php"> Profile </a><hr>
-            <a href="../cashierbills/createbill.php"> Create Bill </a><hr>
-            <a href="../promotion/cashierReadPromotion.php"> Promotions </a><hr>
-            <a href="../Cashier/Cashier View Bill History/CashierViewAllBills.php"> Bill History </a><hr><!--*-->
-            <a href="../Cashier service records/cashierViewService.php"> Vehicle Service Records </a><hr> 
-            <a href="../Cashier product reservation/ViewProductResrvation.php"> Product Reservations </a><hr> 
-            <a href="../appointments/cashierReadsAppointments.php"> Appointments </a><hr>
-            <a href="../Cashier Customer register/cashier register customer.php"> Customer </a><hr>
+            <?php include_once("../Cashier/cashierSideNav.php") ?> 
         </div>
+
 
         <div class="col-16 content">
             <!--main content here-->
