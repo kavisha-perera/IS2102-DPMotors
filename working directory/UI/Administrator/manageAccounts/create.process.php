ba0f4809e1d9adf82387a7549d80aeea5c83f2bf
SewminiWanigarathna <2019is079@stu.ucsc.cmb.ac.lk>
2022-03-29 00:34:16 +0530

    admin side account reg

--- working directory/UI/Administrator/manageAccounts/create.process.php
+++ working directory/UI/Administrator/manageAccounts/create.process.php
@@ -12,28 +12,38 @@ if (isset($_POST["add"])){
     $contact = $_POST["contact"];
     $address = $_POST["address"];
 
+
+    function createCustomer($conn, $fname, $lname,  $email, $nic, $password , $type, $contact, $address){
+
+        $sql = "INSERT INTO users (fname, lname, email, nic, password , type, contact, address) VALUES (?, ?, ?, ?, ?, ?, ?, ?);"; 
+        $stmt = mysqli_stmt_init($conn);
+        if(!mysqli_stmt_prepare($stmt, $sql)){
+            header("location: ../UI/Auth-UI/signUp.php?error=stmtfailed");
+            exit();
+        }
+    
+        $hashedPwd = password_hash($password , PASSWORD_DEFAULT);
+    
+        mysqli_stmt_bind_param($stmt, "ssssssss" , $fname, $lname,  $email, $nic, $hashedPwd , $type, $contact, $address);
+        mysqli_stmt_execute($stmt);
+        mysqli_stmt_close($stmt);
+        echo'<script>alert("New Account added succesfully!");history.go(-1);</script>';
+        exit();   
+    
+    }
+
+
     require_once '../../../includes/dbh.inc.php';
+    createCustomer($conn, $fname, $lname, $email, $nic, $password, $type, $contact, $address);
 
- /*   if (invalidEmail($email) !== false){
-        echo'<script>alert("Invalid email!");history.go(-1);</script>';
-        exit();
-    }
-    if (pwdMatch($password, $confirmpw) !== false){
-        echo'<script>alert("Passwords do not match!");history.go(-1);</script>';
-        exit();
-    }
-    if (emailTaken($conn, $email, $nic) !== false){
-        echo'<script>alert("Entered email/NIC already exists!");history.go(-1);</script>';
-        exit();
-    }*/
-
-    $sql="INSERT INTO users(fname, lname, email, nic, password, type, contact, address) VALUES ('$fname', '$lname', '$email', '$nic', '$password', '$type', '$contact', '$address')";   
+    
+ /*   $sql="INSERT INTO users(fname, lname, email, nic, password, type, contact, address) VALUES ('$fname', '$lname', '$email', '$nic', '$password', '$type', '$contact', '$address')";   
 
     if ($conn->query($sql) === TRUE) {
         echo'<script>alert("New user added succesfully!");history.go(-1);</script>';
       } else {
         echo "Error";
-      }
+      }*/
 }
 
 else {
